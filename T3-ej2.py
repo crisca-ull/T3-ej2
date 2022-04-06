dbdde51feb42f0f1670b720c556615298d578abe
crisca-ull <73931424+crisca-ull@users.noreply.github.com>
2022-04-07 00:33:22 +0100

    Add files via upload

---
+++ T3-ej2.py
@@ -0,0 +1,6 @@
+# peso (en kg) y estatura (en metros), calcule el índice de masa corporal y lo almacene en una variable, e imprima por pantalla la frase Tu índice de masa corporal es donde es el índice de masa corporal calculado redondeado con dos decimales.
+Peso = float(input("¿Cuánto pesas? "))
+Estatura = float(input("¿Cuánto mides? "))
+IMC= str(round(Peso/Estatura**2, 2))
+print("Tu índice de masa corporal es " + IMC)
+
